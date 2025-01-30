da6b74ca069017c67ab28941a6cef78cb2d0e04e
brinley53 <brinleyhull53@ku.edu>
2025-01-30 09:26:09 -0600

    wall detecting logic (top level)

--- garden-pacman/scripts/Enemymovement.cs
+++ garden-pacman/scripts/Enemymovement.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
 
 public partial class Enemymovement : Area2D
 {
@@ -8,73 +10,111 @@ public partial class Enemymovement : Area2D
 	
 	private bool hasPowerUp = false;
 	
-	private int direction = 0; // Direction the enemy is facing; right=0, up=1; left=2; down=3
+	private AnimatedSprite2D sprite;
+	
+	private int direction; // Direction the enemy is facing; right=0, up=1; left=2; down=3
+	
+	private RayCast2D topCollider;
+	private RayCast2D bottomCollider;
+	private RayCast2D leftCollider;
+	private RayCast2D rightCollider;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Position = new Vector2(300, 350); //Initially place the enemy at a certain spot
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		topCollider = GetNode<RayCast2D>("TopCollider");
+		bottomCollider = GetNode<RayCast2D>("BottomCollider");
+		leftCollider = GetNode<RayCast2D>("LeftCollider");
+		rightCollider = GetNode<RayCast2D>("RightCollider");
+		changeDirection(3);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		// Check surroundings for either a wall or a new path to change direction
-		checkSurroundings();
+		// Check surroundings for walls and determine all possible directions of movement
+		List<int> validMoves = findValidMoves();
 		
+		// Find the best direction to go
+		//changeDirection(hasPowerUp ? findPlayer(validMoves) : findEdible(validMoves));
+
 		// Move the sprite
 		move();
 	}
 	
-	private void checkSurroundings() {
-		// Function that checks the surroundings by the enemy to know if there's a wall or a free path to go down
+	private void changeDirection(int dir) {
+		direction = dir;
 		
-		// If the enemy is powered-up, choose to go to the best direction to get to player (without turning around)
-		
-		// If next grid space is a wall, move in a different direction
-		
-		// If next grid space is free, randomly choose to continue in the same direction or go down the new path
-		
-		// Include probability of going to the next powerup
-	}
-	
-	private void changeDirection(string dir) {
 		switch (dir) {
-			case "right":
-				direction = 0;
+			case 0:
+				sprite.Play("right");
 				break;
-			case "left":
-				direction = 2;
+			case 1:
+				sprite.Play("up");
 				break;
-			case "up":
-				direction = 1;
+			case 2:
+				sprite.Play("left");
 				break;
-			case "down":
-				direction = 3;
+			case 3:
+				sprite.Play("down");
 				break;
 			default:
 				break;
 		}
 	}
 	
+	private List<int> findValidMoves() {
+		// Returns a list of all possible directions to go
+		
+		List<int> directions = new List<int>();
+
+		// Find walls
+		if (!isWallRight()) {
+			directions.Add(0);
+		}
+		if (!isWallUp()) {
+			directions.Add(1);
+		}
+		if (!isWallLeft()) {
+			directions.Add(2);
+		}
+		if (!isWallDown()) {
+			directions.Add(3);
+		}
+		
+		return directions;
+	}
+	
+	private bool isWallRight() {
+		return rightCollider.IsColliding();
+	}
+	
+	private bool isWallUp() {
+		return topCollider.IsColliding();
+	}
+	
+	private bool isWallLeft() {
+		return leftCollider.IsColliding();
+	}
+	
+	private bool isWallDown() {
+		return bottomCollider.IsColliding();
+	}
+	
+	private int findPlayer(List<int> validMoves) {
+		// Returns the best direction to go to find the player
+		return 0;
+		
+	}
+	
+	private int findEdible(List<int> validMoves) {
+		// Returns the best direction to go to find the next edible
+		return 1;
+	}
+	
 	private void move() {
 		// Right and down are positive, left and up are negative 
 		Position += new Vector2((direction-1)*Speed*(direction%2-1), (direction-2)*Speed*(direction%2)); // (x, y)
-	}
-	
-	private void moveRight() {
-		Position += new Vector2(1*Speed, 0);
-	}
-	
-	private void moveLeft() {
-		Position += new Vector2(-1*Speed, 0);
-	}
-	
-	private void moveUp() {
-		Position += new Vector2(0, -1*Speed);
-	}
-	
-	private void moveDown() {
-		Position += new Vector2(0, 1*Speed);
 	}
 }

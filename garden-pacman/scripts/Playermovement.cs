1ff68683ee20e96fc6ac3f5fac9522d2ee36cb02
Vegito2367 <tejaaditya.g@gmail.com>
2025-01-30 13:49:31 -0600

    Hitting keys only changes directions

--- garden-pacman/scripts/Playermovement.cs
+++ garden-pacman/scripts/Playermovement.cs
@@ -10,9 +10,17 @@ public partial class Playermovement : CharacterBody2D
 	{
 		Vector2 velocity = Velocity;
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if(direction!=Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed * (float)delta;
+			velocity.Y = direction.Y * Speed * (float)delta;
+		}
+		else
+		{
+			velocity.X = (float) Mathf.MoveToward(Velocity.X,Velocity.X,delta*Speed);
+			velocity.Y = (float) Mathf.MoveToward(Velocity.Y,Velocity.Y,delta*Speed);
+		}
 		
-		velocity.X = direction.X * Speed * (float)delta;
-		velocity.Y = direction.Y * Speed * (float)delta;
 		
 		Velocity = velocity;
 		MoveAndSlide();

636dd5797b50d2f76b16548b2e4e5f430ed55a4a
JoseLeyba <leybajoseluis@ku.edu>
2025-01-30 16:31:30 -0600

    Added the Teleport at the middle of the map borders

---
+++ garden-pacman/Scenario/area_2d.gd
@@ -0,0 +1,6 @@
+extends Area2D
+var teleport_location = Vector2(-238, 4)
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body.name == 'CharacterBody2D') or (body.name == 'Player'):
+		body.global_position = teleport_location

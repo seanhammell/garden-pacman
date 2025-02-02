f111bd4bd7f2bfb9b809cea35d12f4130742af6f
brinley53 <brinleyhull53@ku.edu>
2025-02-01 23:52:29 -0600

    Enemy chases player!!!! easyyy money
    scenario change is only changing the zoom on the camera. feel free to discard scenario change if needed

--- garden-pacman/scripts/Enemymovement.cs
+++ garden-pacman/scripts/Enemymovement.cs
@@ -8,7 +8,7 @@ public partial class Enemymovement : CharacterBody2D
 	[Export]
 	public float Speed { get; set; } = 0.25f;
 	
-	private bool hasPowerUp = false;
+	private bool hasPowerUp = true;
 	
 	private AnimatedSprite2D sprite;
 	
@@ -20,27 +20,42 @@ public partial class Enemymovement : CharacterBody2D
 	private Area2D rightCollider;
 	private Area2D[] colliders;
 	
+	// Booleans to hold the previous state of the colliders (whether they're touching the wall or not)
 	private bool previousRightCollision;
 	private bool previousLeftCollision;
 	private bool previousBottomCollision;
 	private bool previousTopCollision;
+	
+	// To detect and time event changes for the colliders
 	private bool eventOccurred;
 	private double eventTimer;
+	
+	// Player reference
+	private CharacterBody2D player;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Set all the colliders
+		// Get the sprite to change animations
 		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		
+		// Get the player reference
+		player = GetNode<CharacterBody2D>("/root/Scenario/Player");
+		
+		// Get the colliders
 		topCollider = GetNode<Area2D>("TopCollider");
 		bottomCollider = GetNode<Area2D>("BottomCollider");
 		leftCollider = GetNode<Area2D>("LeftCollider");
 		rightCollider = GetNode<Area2D>("RightCollider");
+		colliders = new Area2D[] {rightCollider, topCollider, leftCollider, bottomCollider};
+		
+		// Set the previous state of the colliders
 		previousRightCollision = rightCollider.GetOverlappingBodies().Count > 0;
 		previousLeftCollision = leftCollider.GetOverlappingBodies().Count > 0;
 		previousTopCollision = topCollider.GetOverlappingBodies().Count > 0;
 		previousBottomCollision = bottomCollider.GetOverlappingBodies().Count > 0;
-		colliders = new Area2D[] {rightCollider, topCollider, leftCollider, bottomCollider};
+		
+		// Initialize variables (direction and the event timer)
 		changeDirection(1);
 		eventTimer = 0.0;
 	}
@@ -48,14 +63,17 @@ public partial class Enemymovement : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		double maxTime = 0.3f;
+		// The minimum time before the enemy can change directions again
+		double  minTime = 0.3f;
 		
 		// If event occurred, wait for a bit before testing event occurred again
-		if (eventTimer > maxTime) {
+		if (eventTimer > minTime) { // If the timer is up, allow events to occur again
 			eventOccurred = false;
-		} else if (eventOccurred) {
-			eventTimer += delta;
-			previousRightCollision = isWall(0);
+		} else if (eventOccurred) { // If an event occurred and the timer hasn't gone off, prevent enemy from changing direction
+			eventTimer += delta; // Add time to the timer
+			
+			// Set the previous states of the colliders so they don't think they have changed states
+			previousRightCollision = isWall(0); 
 			previousTopCollision = isWall(1);
 			previousLeftCollision = isWall(2);
 			previousBottomCollision = isWall(3);
@@ -63,26 +81,9 @@ public partial class Enemymovement : CharacterBody2D
 		
 		// Check to see if something changed to cause us to change direction
 		if (!eventOccurred) {
-			if (previousRightCollision != isWall(0)) {
-				previousRightCollision = !previousRightCollision;
-				eventOccurred = true;
-				eventTimer = 0;
-			}
-			if (previousTopCollision != isWall(1)) {
-				previousTopCollision = !previousTopCollision;
-				eventOccurred = true;
-				eventTimer = 0;
-			}
-			if (previousLeftCollision != isWall(2)) {
-				previousLeftCollision = !previousLeftCollision;
-				eventOccurred = true;
-				eventTimer = 0;
-			}
-			if (previousBottomCollision != isWall(3)) {
-				previousBottomCollision = !previousBottomCollision;
-				eventOccurred = true;
-				eventTimer = 0;
-			}
+			// If a collider was previously detecting a wall and it now isn't (or vice versa), indicate that an event occurred
+			eventOccurred = detectChange(previousRightCollision, 0) || detectChange(previousTopCollision, 1) || detectChange(previousLeftCollision, 2) || detectChange(previousBottomCollision, 3);
+			eventTimer = 0; // Reset the timer
 		}
 		
 		
@@ -90,19 +91,34 @@ public partial class Enemymovement : CharacterBody2D
 		if (eventOccurred && eventTimer == 0 || isWall(direction)) {
 			// Check surroundings for walls and determine all possible directions of movement
 			List<int> validMoves = findValidMoves();
-			Random random = new Random();
-			changeDirection(validMoves[random.Next(validMoves.Count)]);
+			Random random = new Random(); // Initialize a random generator
+			
+			if (hasPowerUp) { // If the enemy has the powerup, chase the player
+				List<int> bestMoves = findPlayer(validMoves);
+				changeDirection(bestMoves.Count > 0 ? bestMoves[random.Next(bestMoves.Count)] : validMoves[random.Next(validMoves.Count)]);
+			} else { // Otherwise, randomly move
+				changeDirection(validMoves[random.Next(validMoves.Count)]);
+			}
 		}
-		
-		// Change random to hasPowerUp ? findPlayer(validMoves) : findEdible(validMoves)
 
 		// Move the sprite
 		move();
 	}
+	
+	private bool detectChange(bool previousColliderState, int direction) {
+		// Detect whether the enemy hit a wall or found a new path
+		if (previousColliderState != isWall(direction)) {
+			previousColliderState = !previousColliderState;
+			return true;
+		}
+		return false;
+	}
+	
 	public void die()
 	{
 		QueueFree();
 	}
+	
 	private void changeDirection(int dir) {
 		direction = dir;
 		
@@ -130,27 +146,57 @@ public partial class Enemymovement : CharacterBody2D
 		
 		List<int> oppositeDirection = new List<int>();
 
-		// Find walls
+		// Go through every direction and determine if that direction is open or a wall
 		for (var i = 0; i < 4; i++) {
-			if (Math.Abs(direction-i) == 2) { // don't go backward
-				oppositeDirection.Add(i);
+			if (Math.Abs(direction-i) == 2) { 
+				oppositeDirection.Add(i); // Detect which direction is backwards and don't include it in valid moves
 				continue;
 			}
 			if (!isWall(i)) {
+				// If there isn't a wall in that direction, mark it as valid
 				directions.Add(i);
 			}
 		}
 		
+		// Return the list of valid moves, otherwise tell it to go in the opposite direction
 		return directions.Count > 0 ? directions : oppositeDirection;
 	}
 	
 	private bool isWall(int dir) {
+		// Returns whether the specified collider detects a wall
 		return colliders[dir].GetOverlappingBodies().Count > 0;
 	}
 	
-	private int findPlayer(List<int> validMoves) {
+	private List<int> findPlayer(List<int> validMoves) {
 		// Returns the best direction to go to find the player
-		return 0;
+		List<int> bestDirections = new List<int>();
+		
+		// Find the difference between the player and enemy positions
+		Vector2 posDifference = player.Position - Position;
+		
+		// Determine if the player is to the right or left
+		if (posDifference[0] > 0) {
+			if (validMoves.Contains(0)) {
+				bestDirections.Add(0);
+			}
+		} else if (posDifference[0] < 0) {
+			if (validMoves.Contains(2)) {
+				bestDirections.Add(2);
+			}
+		}
+		
+		// Determine if the player is up or down
+		if (posDifference[1] > 0) {
+			if (validMoves.Contains(3)) {
+				bestDirections.Add(3);
+			}
+		} else if (posDifference[1] < 0) {
+			if (validMoves.Contains(1)) {
+				bestDirections.Add(1);
+			}
+		}
+		
+		return bestDirections;
 	}
 	
 	private int findEdible(List<int> validMoves) {
@@ -159,6 +205,7 @@ public partial class Enemymovement : CharacterBody2D
 	}
 	
 	private void move() {
+		// Move the enemy
 		// Right and down are positive, left and up are negative 
 		Position += new Vector2((direction-1)*Speed*(direction%2-1), (direction-2)*Speed*(direction%2)); // (x, y)
 	}

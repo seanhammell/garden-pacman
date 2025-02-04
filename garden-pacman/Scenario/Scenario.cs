3c0c6368d1a902701e1a661b6a6fe2dfeb3bc902
JoseLeyba <leybajoseluis@ku.edu>
2025-02-03 20:54:14 -0600

    Pellets Left Behind by Player

--- garden-pacman/Scenario/Scenario.cs
+++ garden-pacman/Scenario/Scenario.cs
@@ -4,14 +4,13 @@ using System;
 public partial class Scenario : Node2D
 {
 	private int _score;
-
 	public void NewGame()
 	{
 		GetNode<Timer>("ScoreTimer").Start();
+		GetNode<Timer>("PelletsTimer").Start();
 		_score = 0;
 		var hud = GetNode<Hud>("HUD");
 		hud.UpdateScore(_score);
-
 		var player = GetNode<Playermovement>("Player");
 		player.Reset();
 	}
@@ -20,6 +19,14 @@ public partial class Scenario : Node2D
 	{
 		++_score;
 		GetNode<Hud>("HUD").UpdateScore(_score);
+	}
+	
+		public void OnPelletsTimerTimeout()
+	{
+		var PlayerNode = GetNode<CharacterBody2D>("Player");
+		var PelletsInstance = GetNode<Area2D>("Pellets");
+		PelletsInstance.GlobalPosition = PlayerNode.GlobalPosition;
+		GetTree().CurrentScene.AddChild(PelletsInstance);
 	}
 
 	// Called when the node enters the scene tree for the first time.

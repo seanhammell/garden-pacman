4904883fa907c0a7108e80fe0dc84e6373e5064f
brinley53 <brinleyhull53@ku.edu>
2025-02-04 20:11:57 -0600

    add some design elements mostly

--- garden-pacman/scripts/EnemyPowerup.cs
+++ garden-pacman/scripts/EnemyPowerup.cs
@@ -6,25 +6,25 @@ public partial class EnemyPowerup : Area2D
 	// Get enemy node
 	private CharacterBody2D enemy;
 	
+	private bool isRespawning = true;
 	private double timer; 
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		enemy = GetNode<CharacterBody2D>("/root/Scenario/Enemy");
-		timer = 0.0;
+		timer = 100.0;
 		respawn();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (!this.Visible) {
+		if (!this.Visible && !isRespawning) {
 			timer += delta;
 			if (timer >= (int)enemy.Get("PowerupDuration")) {
 				// Stop the chase & respawn the thang
 				Show();
-				timer = 0.0;
 			}
 		}
 	}
@@ -45,7 +45,7 @@ public partial class EnemyPowerup : Area2D
 			validPosition = true; // Assume position is valid unless we find a conflict
 
 			// Ensure it doesn't spawn in the greenhouse
-			if ((-25 < newPosX && newPosX < 25) && (50 < newPosY && newPosY < 100)) {
+			if ((-25 < newPosX && newPosX < 25) && (-100 < newPosY && newPosY < 100)) {
 				validPosition = false;
 				continue;
 			}
@@ -53,19 +53,23 @@ public partial class EnemyPowerup : Area2D
 		}
 
 		Position = new Vector2(newPosX, newPosY);
+		isRespawning = false;
 	}
 
 	
 	private void OnBodyEntered(Node2D body)
 	{
 		if (body != GetNode<CharacterBody2D>("/root/Scenario/Player") && body != enemy) { // Make sure that the powerup doesn't respawn on top of something
+			Hide();
+			isRespawning = true;
 			respawn();
 		}
 		
 		// If the enemy eats it and it has been the proper amount of time since the last powerup
-		if (body == enemy && timer <= 0.0) {
+		if (body == enemy && timer >= (int)enemy.Get("PowerupDuration")) {
 			// Hide powerup for timer duration
 			Hide();
+			isRespawning = true;
 			respawn();
 			enemy.Call("Powerup");
 			startTimer();
@@ -73,6 +77,6 @@ public partial class EnemyPowerup : Area2D
 	}
 	
 	private void startTimer() {
-		timer = 0;
+		timer = 0.0;
 	}
 }
